5844995003b26d5668e024a07a36ccf249fb8554
Bruno Novais <cardososp@gmail.com>
2025-03-23 22:48:20 -0300

    Add set-dbm-propagation

---
+++ management-scripts/set-dbm-propagation.ps1
@@ -0,0 +1,18 @@
+# Invoke-WebRequest -Uri "https://raw.githubusercontent.com/novasdream/datadog-aas-extension/refs/heads/master/management-scripts/install-extension-with-publishing-profile.ps1" -OutFile "install-extension-with-publishing-profile.ps1"
+param (
+    [Parameter(Mandatory = $true)][string]$SubscriptionId,
+    [Parameter(Mandatory = $true)][string]$ResourceGroup
+)
+
+$rawAllSites = az webapp list --subscription $SubscriptionId -g $ResourceGroup --output json
+
+$allSites = $rawAllSites | ConvertFrom-Json
+
+Foreach ($webapp in @($allSites)) {
+
+    if ($webapp.kind -eq "app") {
+        $SiteName = $webapp.name
+        Write-Host "Setting DD_DBM_PROPAGATION_MODE [${SiteName}]"
+        az webapp config appsettings set -n ${SiteName} -g $ResourceGroup --settings DD_DBM_PROPAGATION_MODE=1 | Out-Null
+    }
+}
